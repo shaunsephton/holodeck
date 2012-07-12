ba1c1cb51228e278c5bbfb1be14f108a9c77b1bc
Shaun Sephton <shaun@28lines.com>
2012-07-12 12:56:39 +0200

    cleanup

--- bitpile/management/commands/syncsharddbs.py
+++ bitpile/management/commands/syncsharddbs.py
@@ -7,7 +7,6 @@ class Command(BaseCommand):
     help = "Create the database tables for bitpile.models.Sample in shards whose tables haven't already been created."
 
     def handle(self, *args, **options):
-
         for database_name, database_settings in settings.DATABASES.items():
             if database_name.startswith('shard_'):
                 print 'Syncing %s...' % database_name
