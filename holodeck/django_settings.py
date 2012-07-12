cb39ffbcaaa1946e4a726f33d7c0c1f0ee4a1174
Shaun Sephton <shaun@28lines.com>
2012-07-12 21:31:20 +0200

    don't use tx, included media/static settings

--- holodeck/django_settings.py
+++ holodeck/django_settings.py
@@ -1,6 +1,11 @@
+from holodeck.settings import *
 from holodeck.utils import generate_shard_database_settings
+import os
+import sys
 
 # Django settings for Holodeck project.
+PATH = os.path.split(os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]))))[0]
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -52,22 +57,22 @@ USE_I18N = True
 USE_L10N = True
 
 # If you set this to False, Django will not use timezone-aware datetimes.
-USE_TZ = True
+USE_TZ = False
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/home/media/media.lawrence.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(PATH, 'static')
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
