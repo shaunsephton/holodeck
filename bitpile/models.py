d161f6bb6187fcf67b9d9da49d36accc74fa3697
Shaun Sephton <shaun@28lines.com>
2012-07-12 17:06:38 +0200

    initial work on general dashboard models

--- bitpile/models.py
+++ bitpile/models.py
@@ -1,5 +1,60 @@
 from django.db import models
+from bitpile.utils import sample_to_shard_mapper
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=255)
+    dashboards = models.ManyToManyField(
+        'bitpile.Dashboard',
+        blank=True,
+        null=True
+    )
+
+    def __unicode__(self):
+        return self.title
+
+
+class Dashboard(models.Model):
+    title = models.CharField(max_length=255)
+    widgets = models.ManyToManyField(
+        'bitpile.Widget',
+        blank=True,
+        null=True
+    )
+    
+    def __unicode__(self):
+        return self.title
+
+
+class Widget(models.Model):
+    title = models.CharField(max_length=255)
+
+
+class LineChartWidget(Widget):
+    metrics = models.ManyToManyField(
+        'bitpile.Metric',
+        blank=True,
+        null=True
+    )
+
+
+class Metric(models.Model):
+    title = models.CharField(max_length=255)
+    sample_interval = models.IntegerField(
+        choices=((1, "Every Minute"),)
+    )
+
+
+class GoogleAnalyticsUniqueUserMetric(Metric):
+    access_token = models.CharField(max_length=64)
 
 
 class Sample(models.Model):
-    value = models.IntegerField()
+    metric_id = models.IntegerField(max_length=64)
+    integer = models.IntegerField()
+    string = models.CharField(max_length=64)
+
+    def save(self, *args, **kwargs):
+        self.full_clean()
+        kwargs.update({'using': 'shard_%s' % sample_to_shard_mapper(self)})
+        super(Sample, self).save(*args, **kwargs)
