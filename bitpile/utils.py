ceffe92455972e34d83d75ea79814218c779d85e
Shaun Sephton <shaun@28lines.com>
2012-07-12 17:05:17 +0200

    clearer signatures, refactor

--- bitpile/utils.py
+++ bitpile/utils.py
@@ -2,14 +2,17 @@ from copy import copy
 import bitpile_settings
 
 
-def shard_mapper():
-    result = {}
-    mapper = bitpile_settings.LOGICAL_SHARDS / len(bitpile_settings.PHYSICAL_SHARDS)
-    
+def logical_to_pysical_shard_mapper(logical_shard_number):
+    return logical_shard_number / (bitpile_settings.LOGICAL_SHARDS / len(bitpile_settings.PHYSICAL_SHARDS))
+
+def generate_shard_database_settings():
+    dbs = {}
     for i in range(0, bitpile_settings.LOGICAL_SHARDS):
-        shard = copy(bitpile_settings.PHYSICAL_SHARDS[i/mapper])
+        shard = copy(bitpile_settings.PHYSICAL_SHARDS[logical_to_pysical_shard_mapper(i)])
         shard_name = 'shard_%s' % i
         shard['NAME'] = '%s_%s' % (shard['NAME_PREFIX'], shard_name)
-        result[shard_name] = shard
+        dbs[shard_name] = shard
+    return dbs
 
-    return result
+def sample_to_shard_mapper(sample):
+    return sample.bucket_id % bitpile_settings.LOGICAL_SHARDS
