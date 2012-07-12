280fb04908ca90dc0353f0b1ce5824552f702a62
Shaun Sephton <shaun@28lines.com>
2012-07-12 21:30:28 +0200

    cleanup

--- holodeck/models.py
+++ holodeck/models.py
@@ -1,3 +1,5 @@
+import uuid
+
 from django.db import models
 from holodeck.utils import sample_to_shard_mapper
 
@@ -9,6 +11,16 @@ class Project(models.Model):
         blank=True,
         null=True
     )
+    api_key = models.CharField(max_length=32, unique=True, blank=True, null=True)
+
+    @classmethod
+    def generate_api_key(cls):
+        return uuid.uuid4().hex
+
+    def save(self, *args, **kwargs):
+        if not self.api_key:
+            self.api_key = Project.generate_api_key()
+        super(Project, self).save(*args, **kwargs)
 
     def __unicode__(self):
         return self.title
@@ -40,6 +52,7 @@ class LineChartWidget(Widget):
 
 class Metric(models.Model):
     title = models.CharField(max_length=255)
+    project = models.ForeignKey('holodeck.Project')
     sample_interval = models.IntegerField(
         choices=((1, "Every Minute"),)
     )
@@ -51,8 +64,9 @@ class GoogleAnalyticsUniqueUserMetric(Metric):
 
 class Sample(models.Model):
     metric_id = models.IntegerField(max_length=64)
-    integer = models.IntegerField()
-    string = models.CharField(max_length=64)
+    integer_value = models.IntegerField()
+    string_value = models.CharField(max_length=64)
+    timestamp = models.DateTimeField()
 
     def save(self, *args, **kwargs):
         self.full_clean()
