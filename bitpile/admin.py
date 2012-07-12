ce88714ca04953eadf7ee17fe6c4e7890b961c1e
Shaun Sephton <shaun@28lines.com>
2012-07-12 17:04:49 +0200

    initial add

---
+++ bitpile/admin.py
@@ -0,0 +1,7 @@
+from bitpile.models import Dashboard, LineChartWidget, Metric, Project
+from django.contrib import admin
+
+admin.site.register(Dashboard)
+admin.site.register(Metric)
+admin.site.register(Project)
+admin.site.register(LineChartWidget)
