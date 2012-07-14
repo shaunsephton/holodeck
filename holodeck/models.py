4fc61a4d8e5661d60d583ac267fb4b214a1de9f5
Shaun Sephton <shaun@28lines.com>
2012-07-14 12:39:18 +0200

    calc widget type choices from widgets.py, rem gametric, pep8

--- holodeck/models.py
+++ holodeck/models.py
@@ -1,8 +1,10 @@
 import uuid
 
 from django.db import models
-from holodeck.utils import metric_to_shard_mapper, sample_to_shard_mapper
+from holodeck.utils import get_widget_type_choices, load_class_by_string, \
+    metric_to_shard_mapper, sample_to_shard_mapper
 from django.contrib.auth.models import User
+
 
 class Dashboard(models.Model):
     name = models.CharField(max_length=255)
@@ -17,30 +19,34 @@ class Metric(models.Model):
     dashboard = models.ForeignKey('holodeck.Dashboard')
     widget_type = models.CharField(
         max_length=64,
-        choices=(('line_chart', 'Line Chart'),)
+        choices=get_widget_type_choices()
     )
-    api_key = models.CharField(max_length=32, unique=True, blank=True, null=True)
+    api_key = models.CharField(
+        max_length=32,
+        unique=True,
+        blank=True,
+        null=True
+    )
 
+    def __unicode__(self):
+        return self.name
 
     @classmethod
     def generate_api_key(cls):
         return uuid.uuid4().hex
 
+    def render(self):
+        return load_class_by_string(self.widget_type)().render(self)
+
+    @property
+    def sample_set(self):
+        return Sample.objects.filter(metric_id=self.id).using(
+            'shard_%s' % metric_to_shard_mapper(self))
+
     def save(self, *args, **kwargs):
         if not self.api_key:
             self.api_key = Metric.generate_api_key()
         super(Metric, self).save(*args, **kwargs)
-    
-    def __unicode__(self):
-        return self.name
-
-    @property
-    def sample_set(self):
-        return Sample.objects.filter(metric_id=self.id).using('shard_%s' % metric_to_shard_mapper(self))
-
-
-class GoogleAnalyticsUniqueUserMetric(Metric):
-    access_token = models.CharField(max_length=64)
 
 
 class Sample(models.Model):
