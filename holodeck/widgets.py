6065c2effa5d2cab49883f453756c13a86cdc38a
Shaun Sephton <shaun@28lines.com>
2012-07-14 15:03:25 +0200

    pie done

--- holodeck/widgets.py
+++ holodeck/widgets.py
@@ -8,6 +8,9 @@ class Widget(object):
     def get_context(self, metric):
         return NotImplementedError
 
+    def get_groups(self, metric):
+        return [group['string_value'] for group in metric.sample_set.all().values('string_value').distinct()]
+
     def render(self, metric):
         context = self.get_context(metric)
         return render_to_string(self.template_name, context)
@@ -17,23 +20,37 @@ class LineChart(Widget):
     template_name = 'holodeck/widgets/line_chart.html'
 
     def get_context(self, metric):
-        groups = [group['string_value'] for group in metric.sample_set.all().values('string_value').distinct()]
-        [{'string_value': u'random'}, {'string_value': u'series1'}, {'string_value': u'series2'}, {'string_value': u'series3'}]
-       
+        groups = self.get_groups(metric)
         grouped_samples = []
         group_maxes = []
-        sample_count = 50
+        sample_count = 25
         for group in groups:
             samples = [(int(time.mktime(sample.timestamp.timetuple()) * 1000), sample.integer_value) for sample in metric.sample_set.filter(string_value=group).order_by('-timestamp')[:sample_count]]
             grouped_samples.append((group, samples))
             group_maxes.append(max([sample[1] for sample in samples]))
-        samples = json.dumps([group[1] for group in grouped_samples])
-
+        samples = json.dumps([{'label': group[0], 'data': group[1]} for group in grouped_samples])
         return {
             'metric': metric,
             'samples': samples,
             'y_max': max(group_maxes) * 1.025,
+            'width': '600px'
         }
 
 class PieChart(Widget):
     name = 'Pie Chart'
+    template_name = 'holodeck/widgets/pie_chart.html'
+
+    def get_context(self, metric):
+        groups = self.get_groups(metric)
+        grouped_samples = []
+        for group in groups:
+            sample = metric.sample_set.filter(string_value=group).order_by('-timestamp')[0]
+            samples = [(0, sample.integer_value)]
+            grouped_samples.append((group, samples))
+        samples = json.dumps([{'label': group[0], 'data': group[1]} for group in grouped_samples])
+
+        return {
+            'metric': metric,
+            'samples': samples,
+            'width': '300px'
+        }
