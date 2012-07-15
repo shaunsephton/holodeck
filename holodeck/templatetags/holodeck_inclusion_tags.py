58a8557a4be48dd18712b76de01e6d580e02def6
Shaun Sephton <shaun@28lines.com>
2012-07-15 12:08:13 +0200

    pass context through, allows for current dashboard higlight

--- holodeck/templatetags/holodeck_inclusion_tags.py
+++ holodeck/templatetags/holodeck_inclusion_tags.py
@@ -4,8 +4,9 @@ from holodeck.models import Dashboard
 register = template.Library()
 
 
-@register.inclusion_tag('holodeck/inclusion_tags/dashboard_dropdown.html')
-def dashboard_dropdown():
-    return {
-        'object_list': Dashboard.objects.all().order_by('name')
-    }
+@register.inclusion_tag('holodeck/inclusion_tags/dashboard_dropdown.html', takes_context=True)
+def dashboard_dropdown(context):
+    context.update({
+        'dashboard_list': Dashboard.objects.all().order_by('name')
+    })
+    return context
