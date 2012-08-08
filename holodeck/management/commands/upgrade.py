0537c324c28f0201f5499f0fb58bbf829e13cf4d
Shaun Sephton <shaun@28lines.com>
2012-08-08 16:13:04 +0200

    pep8

--- holodeck/management/commands/upgrade.py
+++ holodeck/management/commands/upgrade.py
@@ -3,7 +3,8 @@ from django.core.management.base import BaseCommand
 
 
 class Command(BaseCommand):
-    help = 'Creates required database structure and performs pending migrations.'
+    help = 'Creates required database structure and performs pending '\
+           'migrations.'
 
     def handle(self, **options):
         call_command('syncdb', migrate=True)
