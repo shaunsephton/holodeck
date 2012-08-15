bc939ee8fc48c4434e765c8e55c1b0ced0ae097e
Shaun Sephton <shaun@28lines.com>
2012-08-15 16:29:31 +0200

    PUBLIC is no longer used. added validate_share decorator checking share keys

--- holodeck/decorators.py
+++ holodeck/decorators.py
@@ -1,16 +1,24 @@
 from functools import wraps
 
-from django.conf import settings
 from django.core.urlresolvers import reverse
-from django.http import HttpResponseRedirect
-
+from django.http import Http404, HttpResponseRedirect
+from holodeck.models import Dashboard
 
 def login_required(func):
     @wraps(func)
     def wrapped(request, *args, **kwargs):
-        if not settings.PUBLIC:
-            if not request.user.is_authenticated():
-                request.session['_next'] = request.build_absolute_uri()
-                return HttpResponseRedirect(reverse('holodeck-login'))
+        if not request.user.is_authenticated():
+            request.session['_next'] = request.build_absolute_uri()
+            return HttpResponseRedirect(reverse('holodeck-login'))
         return func(request, *args, **kwargs)
+    return wrapped
+
+def validate_share(func):
+    @wraps(func)
+    def wrapped(request, dashboard_id, share_key, *args, **kwargs):
+        try:
+            Dashboard.objects.get(id=dashboard_id, share_key=share_key)
+        except Dashboard.DoesNotExist:
+            raise Http404
+        return func(request, dashboard_id, share_key, *args, **kwargs)
     return wrapped
