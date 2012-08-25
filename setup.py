f1e0603aef4386828edc09e521f53a5d688ab247
Shaun Sephton <shaun@28lines.com>
2012-08-25 14:14:46 +0200

    0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='holodeck',
-    version='0.0.9',
+    version='0.1.0',
     description='Django based simple dashboard system.',
     long_description = open('README.rst', 'r').read() + open('AUTHORS.rst', 'r').read() + open('CHANGELOG.rst', 'r').read(),
     author='Shaun Sephton',
