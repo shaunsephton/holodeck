623b561b15efec4f6b8e207aec39e66b8d645d6a
Shaun Sephton <shaun@28lines.com>
2012-10-21 10:21:10 +0200

    initial add

---
+++ holodeck/migrations/0005_auto__add_field_metric_share_key.py
@@ -0,0 +1,86 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding field 'Metric.share_key'
+        db.add_column('holodeck_metric', 'share_key',
+                      self.gf('django.db.models.fields.CharField')(max_length=32, unique=True, null=True, blank=True),
+                      keep_default=False)
+
+
+    def backwards(self, orm):
+        # Deleting field 'Metric.share_key'
+        db.delete_column('holodeck_metric', 'share_key')
+
+
+    models = {
+        'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        'auth.permission': {
+            'Meta': {'ordering': "('content_type__app_label', 'content_type__model', 'codename')", 'unique_together': "(('content_type', 'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['contenttypes.ContentType']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Group']", 'symmetrical': 'False', 'blank': 'True'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': "orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        'holodeck.dashboard': {
+            'Meta': {'object_name': 'Dashboard'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['auth.User']", 'null': 'True'}),
+            'share_key': ('django.db.models.fields.CharField', [], {'max_length': '32', 'unique': 'True', 'null': 'True', 'blank': 'True'})
+        },
+        'holodeck.metric': {
+            'Meta': {'ordering': "['position', '-id']", 'object_name': 'Metric'},
+            'api_key': ('django.db.models.fields.CharField', [], {'max_length': '32', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'dashboard': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['holodeck.Dashboard']"}),
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '255'}),
+            'position': ('django.db.models.fields.IntegerField', [], {'null': 'True', 'blank': 'True'}),
+            'share_key': ('django.db.models.fields.CharField', [], {'max_length': '32', 'unique': 'True', 'null': 'True', 'blank': 'True'}),
+            'widget_type': ('django.db.models.fields.CharField', [], {'max_length': '64'})
+        },
+        'holodeck.sample': {
+            'Meta': {'unique_together': "(('metric', 'string_value', 'timestamp'),)", 'object_name': 'Sample'},
+            'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'integer_value': ('django.db.models.fields.IntegerField', [], {'default': '0'}),
+            'metric': ('django.db.models.fields.related.ForeignKey', [], {'to': "orm['holodeck.Metric']"}),
+            'string_value': ('django.db.models.fields.CharField', [], {'max_length': '64'}),
+            'timestamp': ('django.db.models.fields.DateTimeField', [], {})
+        }
+    }
+
+    complete_apps = ['holodeck']
---
+++ holodeck/phantomjs/screenshot_8.js
@@ -0,0 +1,18 @@
+input = phantom.args[0];
+output = phantom.args[1];
+width = 620;
+height = 240;
+
+var page = new WebPage();
+page.viewportSize = { width: width, height: height };
+page.clipRect = { width: width, height: height };
+
+page.open(input, function (status) {
+    if (status !== 'success') {
+        phantom.exit(1);
+    }
+    else {
+        page.render(output);
+        phantom.exit();
+    }
+});
